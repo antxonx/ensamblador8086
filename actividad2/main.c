1e661fc25a154d1753e1f197fa84c153cc1a2ee2
antxony <dantonyofcarim@gmail.com>
2020-10-21 11:40:32 -0500

    limite de 46 elemetos

--- actividad2/main.c
+++ actividad2/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#define LIMIT 46
 
 extern unsigned int fib(unsigned int);
 
 int main(void)
 {
-	fib(15);
+    unsigned int input;
+    do{
+        printf("\n\nIngrese un numero menor o igual a 46:\n");
+        scanf("%d", &input);
+    } while(input > LIMIT);
+    printf("fibonacci con %d elementos:\n", input);
+	fib(input);
 	return 0;
 }
